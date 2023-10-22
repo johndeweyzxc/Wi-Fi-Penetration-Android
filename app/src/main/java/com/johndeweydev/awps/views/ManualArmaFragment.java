0d158977ed9c8ff9faecfd01c738a22bfa614934
johndeweyzxc <johndewey02003@gmail.com>
2023-10-22 20:46:39 +0800

    Refactor UI

--- app/src/main/java/com/johndeweydev/awps/views/ManualArmaFragment.java
+++ app/src/main/java/com/johndeweydev/awps/views/ManualArmaFragment.java
@@ -1,5 +1,6 @@
 package com.johndeweydev.awps.views;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,14 +8,18 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import com.google.android.material.button.MaterialButton;
+import com.johndeweydev.awps.R;
 import com.johndeweydev.awps.databinding.FragmentManualArmaBinding;
 
 public class ManualArmaFragment extends Fragment {
 
   private FragmentManualArmaBinding binding;
+  private MaterialButton buttonStartStop;
 
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -26,9 +31,30 @@ public class ManualArmaFragment extends Fragment {
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
+    buttonStartStop = binding.buttonStartStopManualArma;
+
+    Drawable startIcon = ResourcesCompat.getDrawable(
+            getResources(), R.drawable.ic_start_24, null);
+    Drawable stopIcon = ResourcesCompat.getDrawable(
+            getResources(), R.drawable.ic_stop_24, null);
+    int colorRed = ResourcesCompat.getColor(getResources(), R.color.primaryRed, null);
+    int colorPrimary = ResourcesCompat.getColor(getResources(), R.color.primary, null);
 
     binding.materialToolBarManualArma.setOnClickListener(v -> {
       Navigation.findNavController(binding.getRoot()).popBackStack();
+    });
+
+    buttonStartStop.setOnClickListener(v -> {
+
+      if (buttonStartStop.getText().equals("Start")) {
+        buttonStartStop.setText("Stop");
+        buttonStartStop.setIcon(stopIcon);
+        buttonStartStop.setBackgroundColor(colorRed);
+      } else if (buttonStartStop.getText().equals("Stop")) {
+        buttonStartStop.setText("Start");
+        buttonStartStop.setIcon(startIcon);
+        buttonStartStop.setBackgroundColor(colorPrimary);
+      }
     });
   }
 }
--- app/src/main/java/com/johndeweydev/awps/views/autoarmafragment/autoarmascreens/AutoArmaDashboardFragment.java
+++ app/src/main/java/com/johndeweydev/awps/views/autoarmafragment/autoarmascreens/AutoArmaDashboardFragment.java
@@ -1,5 +1,6 @@
 package com.johndeweydev.awps.views.autoarmafragment.autoarmascreens;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,13 +8,17 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.button.MaterialButton;
+import com.johndeweydev.awps.R;
 import com.johndeweydev.awps.databinding.FragmentAutoArmaDashboardBinding;
 
 public class AutoArmaDashboardFragment extends Fragment {
 
   private FragmentAutoArmaDashboardBinding binding;
+  private MaterialButton buttonStartStop;
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
@@ -24,5 +29,26 @@ public class AutoArmaDashboardFragment extends Fragment {
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
+    buttonStartStop = binding.buttonStopStartAutoArmaDashboard;
+
+    Drawable startIcon = ResourcesCompat.getDrawable(
+            getResources(), R.drawable.ic_start_24, null);
+    Drawable stopIcon = ResourcesCompat.getDrawable(
+            getResources(), R.drawable.ic_stop_24, null);
+    int colorRed = ResourcesCompat.getColor(getResources(), R.color.primaryRed, null);
+    int colorPrimary = ResourcesCompat.getColor(getResources(), R.color.primary, null);
+
+    buttonStartStop.setOnClickListener(v -> {
+
+      if (buttonStartStop.getText().equals("Start")) {
+        buttonStartStop.setText("Stop");
+        buttonStartStop.setIcon(stopIcon);
+        buttonStartStop.setBackgroundColor(colorRed);
+      } else if (buttonStartStop.getText().equals("Stop")) {
+        buttonStartStop.setText("Start");
+        buttonStartStop.setIcon(startIcon);
+        buttonStartStop.setBackgroundColor(colorPrimary);
+      }
+    });
   }
 }
